ad94708dac7767e5dcd5780e2783f70c6f9cb35d
Pedro Felipe <pedro@pedorfelipe.com.br>
2016-07-06 18:11:37 -0300

    fix

--- urlify.js
+++ urlify.js
@@ -173,17 +173,8 @@
         // changes, e.g., "Petty theft" to "petty_theft"
         // remove all these words from the string before urlifying
         s = downcode(s);
-
-        var removelist = [
-            "a", "an", "as", "at", "before", "but", "by", "for", "from", "is",
-            "in", "into", "like", "of", "off", "on", "onto", "per", "since",
-            "than", "the", "this", "that", "to", "up", "via", "with"
-        ];
-
-        var r = new RegExp('\\b(' + removelist.join('|') + ')\\b', 'gi');
-        s = s.replace(r, '');
         // if downcode doesn't hit, the char will be stripped here
-        s = s.replace(/[^-\w\s]/g, '');  // remove unneeded chars
+        s = s.replace(/[^-\w\s]/g, '-');  // remove unneeded chars
         s = s.replace(/^\s+|\s+$/g, ''); // trim leading/trailing spaces
         s = s.replace(/[-\s]+/g, '-');   // convert spaces to hyphens
         s = s.toLowerCase();             // convert to lowercase
